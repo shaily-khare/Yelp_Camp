6bc522af08b7d195f9954f1bac02992f548a101e
shaily-khare <shailykhare15@gmail.com>
2017-09-02 10:42:45 -0700

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//============================================================
 var express         = require('express');
 var bodyParser      = require('body-parser');
 var mongoose        = require('mongoose');
@@ -10,30 +11,42 @@ var seedDB          = require('./seeds');
 var Campground      = require('./models/campground');
 var Comment         = require('./models/comment');
 var User            = require('./models/user');
+//============================================================
+
+
 
 // Requiring Routes
+//============================================================
 var campgroundRoutes = require('./routes/campgrounds');
 var commentRoutes    = require('./routes/comments');
 var indexRoutes       = require('./routes/index');
+//============================================================
+
 
 // DB Connection
+//============================================================
 //mongoose.connect('mongodb://localhost/yelp_camp');
 mongoose.connect('mongodb://yelpcamp_mlab:yelpcamp_mlab@ds115214.mlab.com:15214/yelpcamp_sk', { useMongoClient: true });
 // mongodb://<dbuser>:<dbpassword>@ds115214.mlab.com:15214/yelpcamp_sk
-//mongodb://yelpcamp_mlab:yelpcamp_mlab@ds115214.mlab.com:15214/yelpcamp_sk
+//============================================================
 
+
+
+//Express Settings
+//============================================================
 app.use(bodyParser.urlencoded({extended:true}));
-
-// set the view engine to ejs
 app.set("view engine", "ejs");
-
-// make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
 // seedDB();
+//============================================================
+
+
+
 
 // PASSPORT CONFIGURATION
+//============================================================
 app.use(require('express-session')({
   secret:"Lucky is the cutest dog",
   resave:false,
@@ -45,18 +58,29 @@ app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+//============================================================
+
 
 // Middleware
+//============================================================
+//if user is logged in change navbar and display 2 buttons else if no logged in
 app.use(function(req, res, next){
   res.locals.currentUser = req.user;
   res.locals.error = req.flash("error");
   res.locals.success = req.flash("success");
   next();
 });
+//============================================================
 
+
+
+//Use imports from routes folder
+//============================================================
+//with this we don't need to append /campgrounds into following paths i.e /campgrounds/new or /campgrounds/:id
 app.use('/', indexRoutes);
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/comments', commentRoutes);
+//============================================================
 
 // app.listen(3000, function () {
 //   console.log('Yelp Camp Server is running on port 3000...!!!!');
